043431e00b762fc05b799ba29ec794bd79c5ce7c
RythemGoyal <imrythem16@gmail.com>
2025-03-10 10:49:52 +0530

    zod

--- recap/index.js
+++ recap/index.js
@@ -1,35 +1,55 @@
 require("dotenv").config();
 const express = require("express");
+const z = require("zod");
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  const pw = req.headers.pw;
-  res.json({
-    message: pw,
-  });
-});
-
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Json format",
-  });
-});
-
-app.get("/api/:name", (req, res) => {
-  const { name } = req.params;
-  res.send(`Hello ${name}`);
-});
-
-app.listen(PORT, () => {
-  console.log("Port running on port", PORT);
+const val = z.object({
+  username: z.string(),
+  password: z.string(),
 });
 
 app.use(express.json());
 
-app.post("/hello", (req, res) => {
-  console.log(req.body);
-  console.log(req.query);
-  res.send("Done");
+function validation(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username == "rythem" && password == "goyal") {
+    next();
+  } else {
+    next(err);
+  }
+}
+
+app.get("/", (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const r = val.safeParse({ username, password });
+    if (r == true) {
+      res.json({
+        success: true,
+      });
+    } else {
+      res.json({
+        message: "errorr",
+      });
+    }
+  } catch (err) {
+    res.json({
+      message: "error",
+    });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({
+    message: "error",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Port running on port", PORT);
 });
